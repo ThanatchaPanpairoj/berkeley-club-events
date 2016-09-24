67bf64f4e3acc390faa7a25ca22c5ff0db354649
Thanatcha Panpairoj <therealthanatcha@gmail.com>
2016-09-24 20:51:26 +0000

    Set up site.

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,17 @@
 
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+events = ["sample event"]
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == "GET":
+        return render_template("index.html", events=events)
+    
+    events.append(request.form["contents"])
+    return redirect(url_for('index'))
 
