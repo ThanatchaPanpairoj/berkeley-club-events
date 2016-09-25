d7d53cf352dea116fa5c8f51b27c2765756729cf
thomasmunduchira <thomasjojimunduchira@gmail.com>
2016-09-24 21:30:16 -0700

    Update event_finder.py

--- event_finder.py
+++ event_finder.py
@@ -1,10 +1,19 @@
 import facebook
-import json
+from datetime import datetime
+import dateutil.parser
 
 def find_events():
- 
-    graph = facebook.GraphAPI(access_token='EAAEiEv7tZBwUBAIwy3SPJZAMB0pPFzLPzDOowPKNZBcjciWaLfaDOuk1xCFtlJWUBjMcgFjOI5FgX5sjids2lT6r7sdyZBVgchlQL90JNM6Q2SxsGIXviXUtx0fp6gzPGy8m5B77IGjYoeD6pVSK5iKL9jO3gMkJgkfG9pbN8wZDZD', version='2.7')
-    def pp(o):
-        return json.dumps(o, indent=1)
-    return pp(graph.request("search", {'q' : 'Berkeley, CA', 'type' : 'event'}))
-    
+    graph = facebook.GraphAPI(access_token='EAACEdEose0cBAF5rigVzmxs8U481ean1iizEgAf0ocfiyZBjBWPk4zZAvImoJROHIvm9uyGayKx5I0tducx6ZAzDkDAy5kjRjPNLpIXifsQVWnsdzpfLmiqKxqfM2L6ZA4ERmIfaNfdA9KhZAClUr81l5luXkRXNo3KxIqeJfHQZDZD', version='2.7')
+
+    events = graph.request('search', args={'q': 'berkeley club', 'type': 'event'}, method='GET')
+    output = []
+    for event in events['data']:
+        if 'name' in event:
+            output += [event['name']]
+        if 'location' in event:
+            output += [event['location']]
+        if 'start_time' in event and 'end_time' in event:
+            start = dateutil.parser.parse(event['start_time'])
+            end = dateutil.parser.parse(event['end_time'])
+            output += [start.strftime("%I:%M%p") + " - " + end.strftime("%I:%M%p")]
+    return output
