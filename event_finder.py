582b863f1f8bea2a7e8511615f87f3706d859291
thomasmunduchira <thomasjojimunduchira@gmail.com>
2016-09-24 22:01:51 -0700

    Update event_finder.py

--- event_finder.py
+++ event_finder.py
@@ -1,8 +1,12 @@
 import facebook
-from datetime import datetime
 import dateutil.parser
 
 def find_events():
+    """
+    Uses the Facebook Graph API to find UC Berkeley club events and return them in a list.
+    Queries the API for relevant events and then the name, event, and start and end times are 
+    added to the list if they exist.
+    """
     graph = facebook.GraphAPI(access_token='EAACEdEose0cBANKIjzrBn5fI2zZC3MMITLLWZCfLsl2LRiLsaW7rZCPJo6wqevY5vB3cwruIQx1ZAJT0rb6LBcQQt80BGJkVnwJc0nX6ssjuTtDijX8bv0tILVFclMR155TSx9ZBn7TM4bp9r0j6fcZCPV20NRlraS25Qs7glf9AZDZD', version='2.7')
 
     events = graph.request('search', args={'q': 'berkeley club', 'type': 'event'}, method='GET')
